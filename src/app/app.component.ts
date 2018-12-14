5295dc5b5556def4d34815aac0ac2b7220fca6f5
esmyers <emyers@usgs.gov>
2018-12-14 10:58:54 -0600

    start to leaflet map with TS complaints

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import * as L from 'leaflet';
 
@@ -7,10 +7,32 @@ import * as L from 'leaflet';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    public map: any;
+
   title = 'LIQWIDS';
+  ngOnInit(){
+    this.map = L.map("map", {
+        center: L.latLng(40.9, -73.0),
+        zoom: 9,
+        minZoom: 4,
+        maxZoom: 19,
+        renderer: L.canvas()
+    });
 
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.map);
 
+    L.tileLayer.wms('https://www.waterqualitydata.us/ogcservices/ows?', {
+            layers: "wqp_sites",
+            format: "image/png",
+            transparent: true,
+            searchParams: "characteristicname?text=nitrogen;countycode:US:36:059|US:36:103|US:36:081|US:36:047"
+            
+        }).addTo(this.map);
+  }
+    
   showBasemaps = true;
   showFilters = true;
 
