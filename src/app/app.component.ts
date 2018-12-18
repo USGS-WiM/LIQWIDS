e86107c650cd8c6aecedb6d41e36d6674165ed49
esmyers <emyers@usgs.gov>
2018-12-18 09:45:49 -0600

    general organization started, selectable basemaps in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { JsonPipe } from '@angular/common';
 
 import * as L from 'leaflet';
+import { MapService } from './shared/services/map.service';
 
 @Component({
   selector: 'app-root',
@@ -9,39 +12,73 @@ import * as L from 'leaflet';
 })
 export class AppComponent implements OnInit {
     public map: any;
+    public WQP: any;
+    public NWIS: any;
+    public baseLayers: any;
+    public chosenBaseLayer: string;
+    public layer:any;
+    private APIUrl: 'https://www.waterqualitydata.us/Codes/characteristicname?text=nitrogen&mimeType=json';
+    public data: JsonPipe;
+    //private searchParams: "characteristicname?text=nitrogen;countycode:US:36:059|US:36:103|US:36:081|US:36:047";
 
-  title = 'LIQWIDS';
-  ngOnInit(){
-    this.map = L.map("map", {
-        center: L.latLng(40.9, -73.0),
-        zoom: 9,
-        minZoom: 4,
-        maxZoom: 19,
-        renderer: L.canvas()
-    });
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(this.map);
-
-    L.tileLayer.wms('https://www.waterqualitydata.us/ogcservices/ows?', {
-            layers: "wqp_sites",
-            format: "image/png",
-            transparent: true,
-            searchparams: "characteristicname?text=nitrogen;countycode:US:36:059|US:36:103|US:36:081|US:36:047"
-            
-        }).addTo(this.map);
-  }
+    title = 'LIQWIDS';
     
-  showBasemaps = true;
-  showFilters = true;
+    constructor(private _mapService: MapService){ }
+  
+    ngOnInit(){
 
-  expandSidebar = false;
+        this.map = L.map("map", {
+            center: L.latLng(40.9, -73.0),
+            zoom: 9,
+            minZoom: 4,
+            maxZoom: 19,
+            renderer: L.canvas()
+        });
+        this.baseLayers = this._mapService.baseMaps;
+        this.chosenBaseLayer = "Topo";
+        this.map.addLayer(this._mapService.baseMaps[this.chosenBaseLayer]);
+       
+        //typescript complaining about the searchparams option.  Doesn't matter for right now as this isn't the angular way to load anyway.
+        this.WQP = L.tileLayer.wms('https://www.waterqualitydata.us/ogcservices/ows?', {
+                layers: "wqp_sites",
+                format: "image/png",
+                transparent: true,
+                zIndex: 2,
+                searchParams: "characteristicname?text=nitrogen;countycode:US:36:059|US:36:103|US:36:081|US:36:047"
+            }).addTo(this.map);
 
-  filterSearchInput = "";
-  filterSearch = [
-    "One","Two","Three","Four","Five",
-    "Six","Seven","Eight","Nine","Ten"
-  ]
+        this.NWIS = L.tileLayer.wms("https://www.waterqualitydata.us/ogcservices/ows?", {
+                layers: "qw_portal_map:nwis_sites",
+                format: "image/png",
+                transparent: true,
+                zIndex: 2,
+                searchParams: "countycode:US:36:059|US:36:103"
+            }).addTo(this.map); 
+    }//END NgOnInit
+
+    public toggleLayer(newVal: string){
+        this.chosenBaseLayer = newVal;
+       
+        this.map.removeLayer(this._mapService.baseMaps["OpenStreetMap"]);
+        this.map.removeLayer(this._mapService.baseMaps["Topo"]);
+        this.map.removeLayer(this._mapService.baseMaps["Terrain"]);
+        this.map.removeLayer(this._mapService.baseMaps["Satellite"]);
+        this.map.removeLayer(this._mapService.baseMaps["Gray"]); 
+
+        this.map.addLayer(this._mapService.baseMaps[newVal]);
+    }
+
+    
+    
+    showBasemaps = true;
+    showFilters = true;
+
+    expandSidebar = false;
+
+    filterSearchInput = "";
+    filterSearch = [
+        "One","Two","Three","Four","Five",
+        "Six","Seven","Eight","Nine","Ten"
+    ]
 
 }
---
+++ src/app/shared/services/liqwids.service.ts
@@ -0,0 +1,37 @@
+//purpose:     Service for the whole application. setters/getters needed to communicate information with the other parts of the app
+
+import { Injectable } from '@angular/core';
+import { Http, Response, RequestOptions, URLSearchParams } from '@angular/http';
+import { MapService } from '../../shared/services/map.service';
+import { Subject, Observable } from 'rxjs';
+import { map } from "rxjs/operators";
+import 'rxjs/add/operator/catch';
+
+import { Ioptions } from "../interfaces/options.interface";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LiqwidsService {
+    private characteristicUrl: string;
+    private _options: Subject<Array<Ioptions>> = new Subject<Array<Ioptions>>();
+
+    constructor(private _http: Http, private _mapService: MapService) { this.setOptions(); }
+
+
+    //getter
+    public get options(): Observable<Array<Ioptions>> {
+        return this._options.asObservable();
+    } 
+
+    //called from constructor
+    private setOptions(): void {
+        this._http.get('https://www.waterqualitydata.us/Codes/characteristicname?text=nitrogen&mimeType=json')
+            .pipe(map(res => res.json())
+            
+            
+        );
+            
+            
+    }
+}
